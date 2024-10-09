e7d53ad976bf36ea66193372c4f3dd1a5f0e2112
Ri Teii <teiiri@outlook.com>
2024-10-09 16:43:28 +0100

    🐣 Add project template

---
+++ 0000_template/solution.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/* YYYY/MM/DD | _METHOD | T-?? | M-?? */
+void solution() {
+}
+
+int main() {
+    /* Define test parameters */
+    const char* info = "This is a project template. Copy to start.";
+
+    /* Test function */
+    printf("%s\n", info);
+
+    /* Clean up */
+
+    return EXIT_SUCCESS;
+}

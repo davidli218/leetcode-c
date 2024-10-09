6fb3381106967675ebf711504e817793ed1fa56a
Ri Teii <teiiri@outlook.com>
2024-10-09 15:10:47 +0100

    🫧 Polish `2. Add Two Numbers`

--- 0002_add_two_numbers/add_two_numbers.c
+++ 0002_add_two_numbers/add_two_numbers.c
@@ -1,98 +1,69 @@
-//
-// Created by david on 2020/7/27.
-//
-
 #include <stdlib.h>
 #include <stdio.h>
 
 struct ListNode {
     int val;
-    struct ListNode *next;
+    struct ListNode* next;
 };
 
 /* 2020/7/27 | BF | T-12 | M-79 */
-struct ListNode *addTwoNumbers(const struct ListNode *l1, const struct ListNode *l2) {
-    int tmp, carry = 0;
-    struct ListNode *ret_head = (struct ListNode *) malloc(sizeof(struct ListNode));
-    struct ListNode *ret_ptr = ret_head;
+struct ListNode* addTwoNumbers(const struct ListNode* l1, const struct ListNode* l2) {
+    struct ListNode* head_ptr = malloc(sizeof(struct ListNode));
+    struct ListNode* ret_ptr = head_ptr;
+    int carry = 0;
 
-    /* 1. Handle the common part */
-    while (1) {
-        tmp = l1->val + l2->val + carry;
-        ret_ptr->val = tmp % 10;
-        carry = tmp / 10;
+    while (l1 || l2 || carry) {
+        const int val1 = l1 ? l1->val : 0;
+        const int val2 = l2 ? l2->val : 0;
+        const int sum = val1 + val2 + carry;
 
-        l1 = l1->next;
-        l2 = l2->next;
-        if (l1 == NULL || l2 == NULL)
-            break;
+        head_ptr->val = sum % 10;
+        carry = sum / 10;
 
-        ret_ptr = ret_ptr->next = (struct ListNode *) malloc(sizeof(struct ListNode));
-    }
+        if (l1) l1 = l1->next;
+        if (l2) l2 = l2->next;
 
-    /* 2. Handle the remaining list */
-    if ((l1 = (l1 == NULL) ? l2 : l1) != NULL) {
-        while (l1 != NULL) {
-            ret_ptr = ret_ptr->next = (struct ListNode *) malloc(sizeof(struct ListNode));
-
-            tmp = l1->val + carry;
-            ret_ptr->val = tmp % 10;
-            carry = tmp / 10;
-
-            l1 = l1->next;
+        if (l1 || l2 || carry) {
+            head_ptr->next = malloc(sizeof(struct ListNode));
+            head_ptr = head_ptr->next;
         }
     }
 
-    /* 3. Handle the remaining carry */
-    if (carry) {
-        ret_ptr = ret_ptr->next = (struct ListNode *) malloc(sizeof(struct ListNode));
-        ret_ptr->val = carry;
-    }
+    head_ptr->next = NULL;
 
-    /* 4. Terminate the list */
-    ret_ptr->next = NULL;
-
-    return ret_head;
+    return ret_ptr;
 }
 
-void freeLinkedList(struct ListNode *head) {
-    struct ListNode *tmp;
-
+void freeLinkedList(struct ListNode* head) {
     while (head != NULL) {
-        tmp = head;
+        struct ListNode* tmp = head;
         head = head->next;
         free(tmp);
     }
 }
 
-void run(void) {
-    struct ListNode *ret, *tmp;
-
-    /* Define test case */
-    struct ListNode *test_a = (struct ListNode *) malloc(sizeof(struct ListNode));
-    struct ListNode *test_b = (struct ListNode *) malloc(sizeof(struct ListNode));
+int main() {
+    /* Define test parameters */
+    struct ListNode* test_a = malloc(sizeof(struct ListNode));
+    struct ListNode* test_b = malloc(sizeof(struct ListNode));
     test_a->val = 1;
-    test_a->next = (struct ListNode *) malloc(sizeof(struct ListNode));
+    test_a->next = (struct ListNode*)malloc(sizeof(struct ListNode));
     test_a->next->val = 8;
     test_a->next->next = NULL;
     test_b->val = 0;
     test_b->next = NULL;
 
-    /* Test addTwoNumbers function */
-    ret = addTwoNumbers(test_a, test_b);
+    /* Test function */
+    struct ListNode* ret = addTwoNumbers(test_a, test_b);
+    const struct ListNode* tmp = ret;
+    do {
+        printf("%d\n", tmp->val);
+    } while ((tmp = tmp->next) != NULL);
 
-    /* Print return value */
-    tmp = ret;
-    do { printf("%d\n", tmp->val); } while ((tmp = tmp->next) != NULL);
-
-    /* Release memory */
+    /* Clean up */
     freeLinkedList(test_a);
     freeLinkedList(test_b);
     freeLinkedList(ret);
-}
-
-int main() {
-    run();
 
     return EXIT_SUCCESS;
 }
